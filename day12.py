54a59e453ae1a5cd6b20d15eb5e9e2da789e5cba
lhe <gsurplus@inha.edu>
2025-02-26 10:22:07 +0900

    v1.0 day12 seaborn

--- day12.py
+++ day12.py
@@ -1,21 +1,12 @@
-# Assignment
-# v0.9) v0.8의 사이킷런 라이브러리를 주석처리하고 결측치를 산술평균으로 채워 넣으시오.
+# 나이에 따른 생존율 계산
 
 import numpy as np
 import pandas as pd
-# from sklearn.impute import SimpleImputer
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split    #훈련, 검증세트 분할 함수
+from sklearn.neighbors import KNeighborsRegressor       #적용모델 : K 최근점 이웃 회귀모델
 
-df = pd.DataFrame(
-    {
-        'A':[1, 2, np.nan, 4],
-        'B':[np.nan, 2, 3, 4],
-        'C':[1, 2, 3, 4]
-    }
-)
+titanic = sns.load_dataset('titanic')
 
-print(df)
-
-
-mean = df.mean()
-df.fillna(value=mean, inplace=True)
-print(df)
+median_age = titanic['age']

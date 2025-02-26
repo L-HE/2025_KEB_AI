0ac2dc5ad108fa1f946604c19c6f260e8616f15f
lhe <gsurplus@inha.edu>
2025-02-26 13:17:54 +0900

    v1.2 day13 duck typing

--- day13.py
+++ day13.py
@@ -1,8 +1,28 @@
-import pandas as pd
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
 
-data = [1, 7, 5, 2, 8, 3, 6, 4]
-bins = [0, 3, 6, 9]
-labels = ["low", "mid", "high"]
 
-cat = pd.cut(data, bins, labels= labels, right=False)
-print(cat)
+    def walks(self):
+        print("Walk..")
+
+    # java에서는 target_pokemon은 같은 Pokemon type으로 받는 곳이므로
+    # 오류가 난다.
+    # python에서는 그냥 적용됨. type checking을 안해서.
+    def attack(self, target_pokemon):
+        print(f"{self.name}Attack {target_pokemon.name}!")
+
+
+#상속받을 땐 괄호 안에 부모 클래스
+class Pikachu(Pokemon):
+    pass
+
+
+class Agumon:
+    def __init__(self, name):
+        self.name = name
+
+
+agumon = Agumon('아구몬')
+pikachu = Pikachu('피카츄')
+pikachu.attack(agumon)
